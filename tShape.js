772c271ef229614ea8130ff6a1987c43183de49f
LeyuanLoh <leyuan@iastate.edu>
2021-01-08 09:14:11 -0600

    Fixed tShape uncaught syntax error

--- tShape.js
+++ tShape.js
@@ -3,6 +3,6 @@ import { tetris } from './tetris.js'
 export class tShape extends tetris {
     constructor(lastRenderedTime) {
         let shape = [{ x: 4, y: 0 }, { x: 4, y: 1 }, { x: 3, y: 1 }, { x: 5, y: 1 }]
-        super(shape, lastRenderedTime, "legoTShape")]
-}
+        super(shape, lastRenderedTime, "legoTShape")
+    }
 }
